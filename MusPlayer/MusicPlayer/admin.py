5e18a58e287f542c49733de85b28913ac85e9168
volt45 <108761071+volt45@users.noreply.github.com>
2024-04-17 23:02:59 +0200

    Delete MusPlayer directory

--- MusPlayer/MusicPlayer/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from MusicPlayer.models import User, Music
-
-admin.site.register(User)
-admin.site.register(Music)
--- MusPlayer/MusicPlayer/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MusicplayerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'MusicPlayer'
